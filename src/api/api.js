81872847a96e964ee20a7eb0f90978edc9268cf5
Ariel Altaras <ariel.altaras@gmail.com>
2018-06-26 18:52:36 -0700

    added routes for bands

--- src/api/api.js
+++ src/api/api.js
@@ -2,11 +2,10 @@
 
 import express from 'express';
 import Albums from '../models/albums.js';
+import Bands from '../models/bands.js';
 const router = express.Router();
 
-const API_URL = '/api/v1/albums';
-
-router.get(API_URL, (req,res) => {
+router.get('/albums', (req,res) => {
   Albums
     .find()
     .then(albums => res.send(albums))
@@ -20,21 +19,56 @@ router.get('/api/v1/albums/:id', (req,res,next) => {
     .catch( next );
 });
 
-router.post(API_URL, express.json(), (req, res) => {
+router.post('/albums', express.json(), (req, res) => {
   Albums
     .create(req.body)
     .then(album => res.send(album))
     .catch(err => res.send(err));
 });
 
-router.put(API_URL, (req, res) => {
+router.put('/albums', (req, res) => {
   Albums
     .findByIdAndUpdate(req.params.id, req.body)
     .then( albums => res.send(albums) )
     .catch( next );
 });
 
-router.delete(API_URL + '/:id', (req, res) => {
+router.delete('/albums/:id', (req, res) => {
+  Albums
+    .findByIdAndRemove(req.params.id)
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
+});
+
+router.get('/bands', (req,res) => {
+  Albums
+    .find()
+    .then(albums => res.send(albums))
+    .catch(err => res.sendStatus(err));
+});
+
+router.get('/api/v1/bands/:id', (req,res,next) => {
+  Albums
+    .findById(req.params.id)
+    .then( albums => res.send(albums) )
+    .catch( next );
+});
+
+router.post('/bands', express.json(), (req, res) => {
+  Albums
+    .create(req.body)
+    .then(album => res.send(album))
+    .catch(err => res.send(err));
+});
+
+router.put('/bands', (req, res) => {
+  Albums
+    .findByIdAndUpdate(req.params.id, req.body)
+    .then( albums => res.send(albums) )
+    .catch( next );
+});
+
+router.delete('/bands/:id', (req, res) => {
   Albums
     .findByIdAndRemove(req.params.id)
     .then(result => res.send(result))
--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 import router from './api/api.js';
-app.use(router);
+app.use('/api/v1', router);
 
 app.use(notFound);
 
